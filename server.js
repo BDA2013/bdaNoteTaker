94893c1d305340d8499ab35b07fae61ee8c051e1
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-08-22 11:12:06 -0400

    fix routes

--- server.js
+++ server.js
@@ -11,14 +11,6 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
 app.get('/api/notes', (req, res) => res.json(notesdb));
 
 app.post('/api/notes', (req, res) => {
@@ -42,6 +34,14 @@ app.delete('/api/notes/:id', (req, res) => {
   const index = notesdb.indexOf(note);
   notesdb.splice(index, 1);
   res.json(notesdb);
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
